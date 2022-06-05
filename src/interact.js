aff1fa8174beb01940214998f13368703b23351b
Elson Otake <elsonotake@yahoo.com.br>
2022-06-05 18:59:46 -0300

    Update interact.js forEach

--- src/interact.js
+++ src/interact.js
@@ -1,16 +1,14 @@
 const interact = (tasks) => {
-  let completed;
 
-  for (let i = 1; i <= tasks.size(); i += 1) {
-    ({ completed } = tasks.idxTask(i));
-    const checkbox = document.querySelector(`.task_unit input[type=checkbox].idx${i}`);
-    const text = document.querySelector(`.task_unit input[type=text].idx${i}`);
-
-    if (completed) {
+  tasks.tasks.forEach((task, i) => {
+        
+    if (task.completed) {
+      const checkbox = document.querySelector(`.task_unit input[type=checkbox].idx${i + 1}`);
+      const text = document.querySelector(`.task_unit input[type=text].idx${i + 1}`);
       checkbox.checked = true;
       text.classList.add('done');
     }
-  }
-};
+  });
+}
 
 export default interact;
